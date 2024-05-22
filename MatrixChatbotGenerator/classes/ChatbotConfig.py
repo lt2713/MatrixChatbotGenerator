6483e92fd707da00fdc26d9f25c9d1eae31fa1db
Tobias Leiter <134714529+lt2713@users.noreply.github.com>
2024-05-23 00:37:08 +0200

    logging

--- MatrixChatbotGenerator/classes/ChatbotConfig.py
+++ MatrixChatbotGenerator/classes/ChatbotConfig.py
@@ -14,14 +14,14 @@ class ChatbotConfig:
     def get_password(self):
         return self.config['Matrix']['password']
 
-    def print(self):
-        print("Homeserver:", self.get_homeserver())
-        print("User ID:", self.get_user_id())
-        print("Password:", self.get_password())
+    def get(self):
+        return f"Homeserver: {self.get_homeserver()}\t" \
+               f"User ID: {self.get_user_id()}\t" \
+               f"Password: {self.get_password()}\t"
 
 
 # Example usage
 if __name__ == "__main__":
     bot_config = ChatbotConfig()
-    bot_config.print()
+    print(bot_config.get())
 
--- MatrixChatbotGenerator/util/utility_functions.py
+++ MatrixChatbotGenerator/util/utility_functions.py
@@ -1,5 +1,30 @@
 import inspect
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        # Modify the original format function to replace newlines with tabs
+        original = super(CustomFormatter, self).format(record)
+        return original.replace('\n', '\t')
+
+
+def create_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    # Set up a RotatingFileHandler
+    handler = RotatingFileHandler(f'../data/{name}.log', maxBytes=1024 * 1024 * 5, backupCount=5)
+    formatter = CustomFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+    return logger
 
 
 def current_function_name():
     return inspect.stack()[1].function
+
+
