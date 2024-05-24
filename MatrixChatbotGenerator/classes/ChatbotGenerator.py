4ff83e4bb124b541db517440f2f3dfdff4e49da8
Tobias Leiter <134714529+lt2713@users.noreply.github.com>
2024-05-24 14:22:40 +0200

    make chatbot generator use flask app

--- MatrixChatbotGenerator/classes/ChatbotGenerator.py
+++ MatrixChatbotGenerator/classes/ChatbotGenerator.py
@@ -1,8 +1,7 @@
+import requests
 from structures.questions import Questions
-from classes.QTIParser import QTIParser
 from structures.transaction import Transaction
-from classes.QuizBot import Quizbot
-from store.db_operations import *
+from classes.QTIParser import QTIParser
 
 
 class ChatbotGenerator:
@@ -16,18 +15,19 @@ class ChatbotGenerator:
         else:
             self.questions = None
         self.message = ' '
+        self.api_url = 'http://ltquiz.duckdns.org:2713/'
 
     def start(self):
         if not self.transaction or not self.questions or len(self.questions.questions) == 0:
             self.message = 'Chatbot generation failed! Parameter Error.'
             return False
-        if quiz_exists(self.transaction.quiz_name):
+        if self.quiz_exists(self.transaction.quiz_name):
             self.message = 'There already is a Quiz with this name. Please use a different Name'
             return False
         try:
-            add_transaction_as_quiz_to_db(self.transaction)
+            quiz_id = self.add_transaction_as_quiz_to_db(self.transaction)
             for question in self.questions.questions:
-                add_custom_question_to_db(question, self.transaction.identifier)
+                self.add_custom_question_to_db(question, quiz_id)
         except Exception as e:
             self.message = f'Quiz could not be added due to an unexpected Error: {e}'
             return False
@@ -36,6 +36,32 @@ class ChatbotGenerator:
 
     def get_message(self):
         return self.message
+
+    def quiz_exists(self, quiz_name):
+        response = requests.get(f'{self.api_url}/quizzes/{quiz_name}')
+        return response.status_code == 200
+
+    def add_transaction_as_quiz_to_db(self, transaction):
+        quiz_data = {
+            'name': transaction.quiz_name,
+            'messages_per_day': transaction.messages_per_day
+        }
+        response = requests.post(f'{self.api_url}/quizzes', json=quiz_data)
+        if response.status_code != 201:
+            raise Exception('Failed to add quiz')
+        return response.json()['id']
+
+    def add_custom_question_to_db(self, question, quiz_id):
+        question_data = {
+            'type': question.type,
+            'text': question.text,
+            'answers': [{'identifier': ans.identifier, 'text': ans.text, 'is_correct': ans.correct} for ans in
+                        question.answers],
+            'feedback': [{'identifier': fb.identifier, 'text': fb.text} for fb in question.feedback]
+        }
+        response = requests.post(f'{self.api_url}/quizzes/{quiz_id}/questions', json=question_data)
+        if response.status_code != 201:
+            raise Exception('Failed to add question')
 
 
 if __name__ == '__main__':
--- MatrixChatbotGenerator/store/flask_app.py
+++ MatrixChatbotGenerator/store/flask_app.py
@@ -19,6 +19,36 @@ def fetch_questions_for_quiz(quiz_id):
     return jsonify([{'id': question.id, 'type': question.type, 'text': question.text} for question in questions])
 
 
+@app.route('/api/quizzes/<quiz_name>', methods=['GET'])
+def quiz_exists(quiz_name):
+    quiz = session.query(Quiz).filter_by(name=quiz_name).first()
+    if quiz:
+        return jsonify({'id': quiz.id}), 200
+    return jsonify({'error': 'Quiz not found'}), 404
+
+
+@app.route('/api/quizzes', methods=['POST'])
+def add_quiz():
+    data = request.get_json()
+    new_quiz = Quiz(name=data['name'], messages_per_day=data['messages_per_day'])
+    session.add(new_quiz)
+    session.commit()
+    return jsonify({'id': new_quiz.id}), 201
+
+
+@app.route('/api/quizzes/<quiz_id>/questions', methods=['POST'])
+def add_question(quiz_id):
+    data = request.get_json()
+    new_question = Question(
+        quiz_id=quiz_id,
+        type=data['type'],
+        text=data['text']
+    )
+    session.add(new_question)
+    session.commit()
+    return jsonify({'id': new_question.id}), 201
+
+
 @app.route('/helloworld', methods=['GET'])
 def helloworld():
     return jsonify('Hello World')
