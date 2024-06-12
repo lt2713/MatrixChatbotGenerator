1bd46c914b85db0c255237108ee3caaed1888749
Tobias Leiter <134714529+lt2713@users.noreply.github.com>
2024-06-12 15:40:22 +0200

    test connection

--- MatrixChatbotGenerator/ui/ConfigWindow.py
+++ MatrixChatbotGenerator/ui/ConfigWindow.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import messagebox
 import configparser
 import os
+
+import requests
+from requests.auth import HTTPBasicAuth
+
 from classes.ConfigManager import ConfigManager
 
 
@@ -28,10 +32,16 @@ class ConfigWindow:
         tk.Label(self.root, text="Password:").grid(row=2, column=0, padx=10, pady=5)
         self.password_entry = tk.Entry(self.root, width=40, show="*")
         self.password_entry.grid(row=2, column=1, padx=10, pady=5)
+        pw = ConfigManager.decrypt_password(self.config['Db']['password'])
+        self.password_entry.insert(0, pw)
 
         tk.Button(self.root, text="Cancel", command=self.root.destroy).grid(row=3, column=0, padx=10,
                                                                             pady=10, sticky='e')
         tk.Button(self.root, text="Save", command=self.on_save).grid(row=3, column=1, padx=10, pady=10, sticky='w')
+
+        if config_name == 'Db':
+            tk.Button(self.root, text="Test Connection", command=self.test_connection).grid(row=4, column=1, padx=10,
+                                                                                        pady=10, sticky='w')
 
     def on_save(self):
         server = self.server_entry.get().strip()
@@ -55,6 +65,24 @@ class ConfigWindow:
             self.root.mainloop()
         except Exception as e:
             print(f'An Error occurred in the config window: {e}')
+
+    def test_connection(self):
+        server = self.server_entry.get().strip()
+        user_id = self.user_id_entry.get().strip()
+        password = self.password_entry.get().strip()
+
+        if not server or not user_id or not password:
+            messagebox.showerror("Input Error", "All fields are required.")
+            return
+
+        try:
+            response = requests.get(f'{server}/helloworld', auth=HTTPBasicAuth(user_id, password))
+            if response.status_code == 200:
+                messagebox.showinfo("Success", "Connection successful!")
+            else:
+                messagebox.showerror("Error", f"Connection failed: {response.status_code}")
+        except Exception as e:
+            messagebox.showerror("Error", f"Connection failed: {e}")
 
 
 if __name__ == '__main__':
