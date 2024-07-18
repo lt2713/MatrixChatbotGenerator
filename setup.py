80fc300f4de3e3a593d23c0ff97575398602013a
Tobias Leiter <134714529+lt2713@users.noreply.github.com>
2024-07-18 16:10:22 +0200

    new dist

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # Script to make the project installable with pip
-# After running this script:
+# After editing this script:
 # build the package:
 # python setup.py sdist bdist_wheel
 # upload the package:
 # twine upload dist/*
 # done, installable with:
-# pip install mci-dibse-lt2713-matrix-chatbot-generator==1.0.4
+# pip install mci-dibse-lt2713-matrix-chatbot-generator==1.0.5
 from setuptools import setup, find_packages
 
 setup(
     name="mci_dibse_lt2713_matrix_chatbot_generator",
-    version="1.0.4",
+    version="1.0.5",
     author="Tobias Leiter",
     author_email="lt2713@mci4me.at",
     description="Package to create a Matrix Chatbot",
